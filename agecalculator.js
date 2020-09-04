1107cc2986e57ae68dc99893355322355255b662
Z Xian <alex.xian@hotmail.com>
2020-09-04 05:42:27 +0000

    w

---
+++ agecalculator.js
@@ -0,0 +1,6 @@
+function ageCalculator(name, yearOfBirth, currentYear){
+  var age = currentYear - yearOfBirth
+  //var epic = name + " is " + age + " years old."
+  return name + " is " + age + " years old."
+}
+console.log(ageCalculator("dfd",1980,2000))
