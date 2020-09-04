cd5c1b18e8fee6a34a9c75d37c0d090e593f8c3a
Z Xian <alex.xian@hotmail.com>
2020-09-04 06:31:30 +0000

    k

---
+++ howManyHundreds.js
@@ -0,0 +1,9 @@
+function howManyHundreds (num) {
+  var division = num / 100;
+  return Math.floor(division);
+}
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
